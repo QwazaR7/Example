77937f410e7f6ea393f469beb66d86d0db307dba
Quasar <puzyrkovv@yandex.ru>
2023-01-28 12:40:50 +0700

    Added Example005

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "влад")
+{
+    Console.WriteLine("Ура, это же Влад!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
