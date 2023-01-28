20a94f948e7ee08a952d87e3209d09b576ec9268
Quasar <puzyrkovv@yandex.ru>
2023-01-28 12:49:47 +0700

    Added Example006

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 5;
+int b = 9;
+int c = 3;
+int d = 11;
+int e = 25;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine("Максимальное число:");
+Console.WriteLine(max);
